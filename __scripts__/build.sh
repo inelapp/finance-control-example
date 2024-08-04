cb1340b60a686896f60141e24a81a53bc299da34
Rodrigo Chumpitaz <chumpitazr130@gmail.com>
2024-08-04 18:28:15 -0500

    update build

--- __scripts__/build.sh
+++ __scripts__/build.sh
@@ -1,15 +1,15 @@
-echo "building..."
+echo "Building..."
 
+# Navega a la raíz del proyecto
 cd "$(dirname "$0")/.."
 
-echo "Installing all dependecies..."
-yarn install;
+echo "Installing all dependencies..."
+yarn install
 
 echo "Installing dependencies and building in commons..."
-cd commons && yarn install && yarn build;
+cd commons && yarn install && yarn build
 
 echo "Installing dependencies in finance-api..."
-cd ../finance-api && yarn install && yarn build;
-
+cd ../finance-api && yarn install && yarn build
 
 echo "Build complete."
