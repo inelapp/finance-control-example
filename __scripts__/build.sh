657281b1f4e5af977369c32a8dcbc534f3842e2a
Rodrigo Chumpitaz <chumpitazr130@gmail.com>
2024-08-04 18:58:07 -0500

    feat update build config

--- __scripts__/build.sh
+++ __scripts__/build.sh
@@ -3,9 +3,18 @@ echo "Building..."
 # Navega a la raíz del proyecto
 cd "$(dirname "$0")/.."
 
+# Elimina los directorios node_modules
+echo "Removing node_modules directories..."
+rm -rf node_modules
+
 echo "Installing all dependencies..."
 yarn install
 
+# Elimina los directorios node_modules en commons y finance-api
+rm -rf commons/node_modules
+rm -rf finance-api/node_modules
+
+# Navega a los proyectos commons y finance-api
 echo "Installing dependencies and building in commons..."
 cd commons && yarn install && yarn build
 
